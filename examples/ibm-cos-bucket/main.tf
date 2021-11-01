256c4c53de18dc3798da596a7eae331f0f25d555
Olavo Borges <olavoborges@gmail.com>
2021-11-01 14:34:20 -0300

    changed cos name

--- examples/ibm-cos-bucket/main.tf
+++ examples/ibm-cos-bucket/main.tf
@@ -2,7 +2,7 @@ data "ibm_resource_group" "cos_group" {
   name = var.resource_group_name
 }
 resource "ibm_resource_instance" "cos_instance" {
-  name              = "cos-instance"
+  name              = "cos-instance-olavo"
   resource_group_id = data.ibm_resource_group.cos_group.id
   service           = "cloud-object-storage"
   plan              = "standard"
