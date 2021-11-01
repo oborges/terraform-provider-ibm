78aabcb477aca0333669fbda99bbc732bc7115e1
oborges <57798855+oborges@users.noreply.github.com>
2021-11-01 14:43:03 -0300

    Update main.tf

--- examples/ibm-cos-bucket-object/main.tf
+++ examples/ibm-cos-bucket-object/main.tf
@@ -3,7 +3,7 @@ data "ibm_resource_group" "cos_group" {
 }
 
 resource "ibm_resource_instance" "cos_instance" {
-  name              = "cos-instance"
+  name              = "cos-instance-olavo"
   resource_group_id = data.ibm_resource_group.cos_group.id
   service           = "cloud-object-storage"
   plan              = "standard"
